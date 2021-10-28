c384dfa2cfb63257a2f46122278883856aaf4d65
J Angel Glez <a01552274@itesm.mx>
2021-10-28 10:58:20 -0500

    Checking min and max values

--- actividad.py
+++ actividad.py
@@ -3,5 +3,15 @@ import pandas as pd
 
 df = pd.read_csv("ulabox.csv")
 
+# printing data frame
+# print(df)
 
+# printing min values
+# print("Rango inferior\n")
+# print(df.min())
 
+# printing max values
+# print("Rango superior\n")
+# print(df.max())
+
+print(df.describe())
